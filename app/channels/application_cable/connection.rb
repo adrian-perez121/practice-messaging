baf8bcfdc30ab86da0a6ef2764f68dcfa9778562
Adrian Perez <adrian1804perez@gmail.com>
2024-08-15 23:20:41 -0700

    Create connection for user

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,21 @@
 module ApplicationCable
+  # Like the name says, this method under ONLY establishes a connection. For other things you have to go into
+  # a channel.
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+
+    def find_verified_user
+      if verified_user = env['warden'].user
+        verified_user
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
