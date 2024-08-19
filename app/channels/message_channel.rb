76501b8c2dff8eeae0e382acd5111d0e0e3502a9
Adrian Perez <adrian1804perez@gmail.com>
2024-08-19 09:09:50 -0700

    Add recieve methods for messages

--- app/channels/message_channel.rb
+++ app/channels/message_channel.rb
@@ -7,4 +7,10 @@ class MessageChannel < ApplicationCable::Channel
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
+
+  def receive(data)
+    # Send the message right back to the subscribers of the broadcast
+    data['user'] = current_user # So our js file knows who the user is
+    ActionCable.server.broadcast('message', data)
+  end
 end
