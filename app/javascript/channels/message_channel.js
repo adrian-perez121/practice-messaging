1ba04abc9f446272f99768862cd26592ddf0f648
Adrian Perez <adrian1804perez@gmail.com>
2024-08-19 09:30:42 -0700

    Remove preventing the default

--- app/javascript/channels/message_channel.js
+++ app/javascript/channels/message_channel.js
@@ -33,7 +33,6 @@ document.addEventListener("turbo:load", () => {
   let form = document.querySelector('#message-form')
   if (form) {
     form.addEventListener('submit', (e) => {
-      e.preventDefault()
       let messageInput = document.querySelector('#message-input').value
       if (messageInput == '') return;
       const message = {
