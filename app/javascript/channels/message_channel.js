f3dbc03612b4070d0ed71b04622de3eb1403a4bb
Adrian Perez <adrian1804perez@gmail.com>
2024-08-19 09:43:15 -0700

    Comment out unneeded code...

--- app/javascript/channels/message_channel.js
+++ app/javascript/channels/message_channel.js
@@ -11,6 +11,7 @@ const messageChannel = consumer.subscriptions.create("MessageChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
+    console.log(data)
     const messageDisplay = document.querySelector('#message-display')
     messageDisplay.insertAdjacentHTML('beforeend', this.template(data))
   },
@@ -21,7 +22,7 @@ const messageChannel = consumer.subscriptions.create("MessageChannel", {
                 <p>${data.user.email}</p>
               </div>
               <div class="message-body">
-                <p>${data.message.body}</p>
+                <p>${data.body}</p>
               </div>
             </article>`
   }
@@ -29,19 +30,19 @@ const messageChannel = consumer.subscriptions.create("MessageChannel", {
 
 // This isn't apart of the creation so it goes outside
 
-document.addEventListener("turbo:load", () => {
-  let form = document.querySelector('#message-form')
-  if (form) {
-    form.addEventListener('submit', (e) => {
-      let messageInput = document.querySelector('#message-input').value
-      if (messageInput == '') return;
-      const message = {
-        body: messageInput // We store the message...
-      }
-
-      messageChannel.send({message: message}) // ...and then we send it off
-    })
-  }
-})
+// document.addEventListener("turbo:load", () => {
+//   let form = document.querySelector('#message-form')
+//   if (form) {
+//     form.addEventListener('submit', (e) => {
+//       let messageInput = document.querySelector('#message-input').value
+//       if (messageInput == '') return;
+//       const message = {
+//         body: messageInput // We store the message...
+//       }
+//
+//       messageChannel.send({message: message}) // ...and then we send it off
+//     })
+//   }
+// })
 
 
